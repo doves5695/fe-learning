a65bc186d86dd3d14687da6f68aa123e8deb64cf
jingrui <790150796@qq.com>
2023-02-19 22:12:12 +0800

    扫雷

--- 小作业/扫雷/config.js
+++ 小作业/扫雷/config.js
@@ -1,6 +1,10 @@
 var tr = 9;
 var td = 9;
-var timeStop = null;
-var maxMine = 6;
+// var timer = null;
+var timer = null;
+var maxMine = 8;
 // var mineObj = {};
+//棋盘的坐标
 var minePos = {};
+var mine = {};
+// var coordinateCol = {};
---
+++ 小作业/扫雷/minesweeper.js
@@ -0,0 +1,153 @@
+//画棋盘
+function drawboard() {
+    for(var i=0; i < tr; i ++) {
+        var ul = document.createElement("ul");
+        document.querySelector(".content").append(ul);
+        for(var j=0; j < td; j ++) {
+            var li = document.createElement("li");
+            ul.appendChild(li);
+        }
+    }
+}
+drawboard();
+
+//画地雷
+function initMine() {
+    var sign = 0;
+    while(sign < maxMine) {
+        var mineX = Math.floor(Math.random()*td);
+        var mineY = Math.floor(Math.random()*tr);
+        if(!mine[mineX+','+mineY]) {
+            mine[mineX+','+mineY] = true;
+            sign++;
+        }
+    }
+    console.log(mine);
+    for(var j = 0; j < tr; j ++) {
+        var ulDom = document.getElementsByTagName("ul")[j];
+        for(var i = 0; i < td; i ++) {
+            var liDom = ulDom.getElementsByTagName("li")[i];
+            minePos[i+','+j] = {isMine : "NO"};
+            if(mine[(mineX = i)+","+(mineY = j)]) {
+                minePos[i+","+j] = {isMine : "YES"};
+            }
+        }
+    }
+    console.log(minePos);
+}
+// initMine();
+
+function bindEvent() {
+   for(var j = 0; j < tr; j ++) {
+    for(var i = 0; i < td; i ++) {
+        (function(x,y){
+            var ulCol = document.getElementsByTagName("ul")[y];
+            var liCol = ulCol.getElementsByTagName("li")[x];
+            liCol.onmousedown = function (e) {
+                if(e.button == 0) {
+                    if(mine[x+","+y]) {
+                        liCol.style.background = 'url("../扫雷/img/lei.jpg") center/contain no-repeat';
+                        return;
+                    } else {
+                    var coordinateCol = ljggCoordinate(x,y);
+                    var count = 0;
+                    for(var j = coordinateCol.lowY; j <= coordinateCol.maxY; j ++) {
+                        for(var i = coordinateCol.lowX; i <= coordinateCol.maxX; i ++) {
+                            if(mine[i+','+j]) {
+                                count ++;
+                                liCol.style.background = "white";
+                                liCol.innerHTML = count;
+                            } else if(count == 0) {              
+                                liCol.innerHTML = "";
+                                liCol.style.background = "white";
+                            }
+                        } 
+                    }                 
+                    }
+                } else if(e.button == 2) {
+                    liCol.style.background = 'url("../扫雷/img/qizi.jpg") center/contain no-repeat';
+                }
+            }
+        })(i,j);
+    }
+}
+  function ljggCoordinate(x,y) {
+    var lowX = x - 1 < 0 ? x : x - 1;
+    var lowY = y - 1 < 0 ? y : y - 1;
+    var maxX = x + 1 > td ? td - 1 : x + 1;
+    var maxY = y + 1 > tr ? tr - 1 : y + 1;
+    return {lowX,lowY,maxX,maxY}; 
+ }
+}
+bindEvent();
+
+//取消默认事件
+document.oncontextmenu = function (e) {
+    e.preventDefault();
+}
+
+//计时器
+function clock() {
+    var timeDom = document.getElementsByClassName("time")[0];
+    var second = 0;
+    var minute = 0;
+    timer = setInterval(function () {
+        second ++;
+        if(second === 60) {
+            minute++;
+            second = 0;
+        }
+        if(minute < 10 && second < 10) {
+            timeDom.value = "时间:" + "0" + minute + ":" + "0" + second;
+        } else if (minute < 10 && second >= 10) {
+            timeDom.value = "时间:" + "0" + minute + ":" + second;
+        } else if (minute >= 10 && second < 10) {
+            timeDom.value = "时间:" + minute + ":" + "0" + second;
+        } else if (minute >= 10 && second >= 10) {
+            timeDom.value = "时间:" + minute + ":" + second;
+        }
+    },1000);
+    button();
+}
+// clock();
+//按钮
+function button() {
+    var timeDom = document.getElementsByClassName("time")[0];
+    var btn = document.getElementsByTagName("button")[0];
+    btn.onmousedown = function(e) {
+        if(e.button == 0) {
+            // clearInterval(timer);
+            if(window.confirm("游戏已经暂停,是否继续")) {
+                clock();
+            } else {
+                minute = 0;
+                second = 0;
+                timeDom.value = "时间:" + "0" + minute + ":" + "0" + second;
+                clearInterval(timer);
+            }
+        } else if(e.button == 2) {
+            second = 0;
+            minute = 0;
+            timeDom.value = "时间:" + "0" + minute + ":" + "0" + second;
+            clearInterval(timer);
+            // initMine();
+        }
+    }
+}
+// button();
+//得分
+// function point () {
+//     var pointDom = document.getElementsByClassName("point")[0];
+//     var Flags = 0;
+//     while(Flags < maxMine) {
+    
+//     }
+// }
+window.onload = function () {
+    if(window.confirm("是否开始游戏")) {
+        initMine();
+        clock();
+    } else {
+        alert("??????");
+    }
+}
