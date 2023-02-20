ba6d6808a200f20c63e775d56e9c583f56848455
jingrui <790150796@qq.com>
2023-02-20 22:21:34 +0800

    扫雷666

--- 小作业/扫雷/config.js
+++ 小作业/扫雷/config.js
@@ -2,7 +2,7 @@ var tr = 9;
 var td = 9;
 // var timer = null;
 var timer = null;
-var maxMine = 8;
+var maxMine = 6;
 // var mineObj = {};
 //棋盘的坐标
 var minePos = {};
--- 小作业/扫雷/minesweeper.js
+++ 小作业/扫雷/minesweeper.js
@@ -56,16 +56,35 @@ function bindEvent() {
                             if(mine[i+','+j]) {
                                 count ++;
                                 liCol.style.background = "white";
-                                liCol.innerHTML = count;
-                            } else if(count == 0) {              
-                                liCol.innerHTML = "";
-                                liCol.style.background = "white";
-                            }
-                        } 
-                    }                 
+                                liCol.innerHTML = count === 0 ? "" : count;           
+                            } 
+                        }
                     }
-                } else if(e.button == 2) {
-                    liCol.style.background = 'url("../扫雷/img/qizi.jpg") center/contain no-repeat';
+                }
+                    if(count === 0) {
+                        for(var j = coordinateCol.lowY; j <= coordinateCol.maxY; j ++) {
+                            for(var i = coordinateCol.lowX; i <= coordinateCol.maxX; i ++) {
+                                if(mine[i+','+j]){
+                                    count++;
+                                }
+                                    var ullittle = document.getElementsByTagName("ul")[j];
+                                    var lilittle = ullittle.getElementsByTagName("li")[i];
+                                    lilittle.style.background = "white";
+                                    lilittle.innerHTML = count === 0 ? "" : count;
+                            }
+                        }
+                    } 
+            } else if(e.button == 2) {
+                liCol.style.background = 'url("../扫雷/img/qizi.jpg") center/contain no-repeat';
+                liCol.onmousedown = false;
+                var pointDom = document.getElementsByClassName("point")[0];
+                if(mine[x+','+y]) {
+                    pointDom.value = "剩余雷数:" + --maxMine;
+                    if(pointDom.value == "剩余雷数:" + "0") {
+                        alert("游戏结束");
+                        clearInterval(timer);
+                    }
+                }
                 }
             }
         })(i,j);
@@ -74,23 +93,22 @@ function bindEvent() {
   function ljggCoordinate(x,y) {
     var lowX = x - 1 < 0 ? x : x - 1;
     var lowY = y - 1 < 0 ? y : y - 1;
-    var maxX = x + 1 > td ? td - 1 : x + 1;
-    var maxY = y + 1 > tr ? tr - 1 : y + 1;
+    var maxX = x + 1 >= td ? td - 1 : x + 1;
+    var maxY = y + 1 >= tr ? tr - 1 : y + 1;
     return {lowX,lowY,maxX,maxY}; 
  }
 }
 bindEvent();
-
 //取消默认事件
 document.oncontextmenu = function (e) {
     e.preventDefault();
 }
 
 //计时器
+var timeDom = document.getElementsByClassName("time")[0];
+var second = 0;
+var minute = 0;
 function clock() {
-    var timeDom = document.getElementsByClassName("time")[0];
-    var second = 0;
-    var minute = 0;
     timer = setInterval(function () {
         second ++;
         if(second === 60) {
@@ -106,17 +124,16 @@ function clock() {
         } else if (minute >= 10 && second >= 10) {
             timeDom.value = "时间:" + minute + ":" + second;
         }
-    },1000);
-    button();
+    },100);
 }
 // clock();
 //按钮
 function button() {
-    var timeDom = document.getElementsByClassName("time")[0];
+    // var timeDom = document.getElementsByClassName("time")[0];
     var btn = document.getElementsByTagName("button")[0];
     btn.onmousedown = function(e) {
         if(e.button == 0) {
-            // clearInterval(timer);
+            clearInterval(timer);
             if(window.confirm("游戏已经暂停,是否继续")) {
                 clock();
             } else {
@@ -130,19 +147,19 @@ function button() {
             minute = 0;
             timeDom.value = "时间:" + "0" + minute + ":" + "0" + second;
             clearInterval(timer);
+            for(var j = 0; j < tr; j ++) {
+                var ulAll = document.getElementsByTagName("ul")[j];
+                for(var i = 0; i < td; i ++) {
+                    var liAll = ulAll.getElementsByTagName("li")[i];
+                    liAll.style.background = "#7E8B92";
+                    liAll.innerHTML = "";
+                }
+            }
             // initMine();
         }
     }
 }
-// button();
-//得分
-// function point () {
-//     var pointDom = document.getElementsByClassName("point")[0];
-//     var Flags = 0;
-//     while(Flags < maxMine) {
-    
-//     }
-// }
+button();
 window.onload = function () {
     if(window.confirm("是否开始游戏")) {
         initMine();
